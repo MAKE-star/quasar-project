31d2a9fbebdb9babaf8b44993076a11212e91588
Mayopo <adeoyemayopoelijah@gmail.com>
2025-07-10 23:16:13 +0100

    postgress fix

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -1,13 +1,13 @@
-import { boot } from 'quasar/wrappers'
-import axios from 'axios'
+import { boot } from 'quasar/wrappers';
+import axios from 'axios';
 
 const api = axios.create({
   baseURL: 'https://quasar-backend-o2c9.onrender.com',
-})
+});
 
 export default boot(({ app }) => {
-  app.config.globalProperties.$axios = axios
-  app.config.globalProperties.$api = api
-})
+  app.config.globalProperties.$axios = axios;
+  app.config.globalProperties.$api = api;
+});
 
-export { api }
+export { api };
