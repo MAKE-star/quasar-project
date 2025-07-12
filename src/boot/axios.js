07942fe0ed48c3c612515e8d605583e7a8252cd3
Mayopo <adeoyemayopoelijah@gmail.com>
2025-07-12 09:22:41 +0100

    backend update with user details

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -2,7 +2,8 @@ import { boot } from 'quasar/wrappers'
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://quasar-backend-o2c9.onrender.com/',
+  // baseURL: 'https://quasar-backend-o2c9.onrender.com/',
+  baseURL: 'http://localhost:3000',
 })
 
 export default boot(({ app }) => {
